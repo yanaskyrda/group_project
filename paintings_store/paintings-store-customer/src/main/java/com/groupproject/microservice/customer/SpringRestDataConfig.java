2124933003bbd1d2d4aaccbb24f7816564050e88
Alexander Totskiy <velly@macpaw.com>
2021-12-08 23:14:12 +0200

    packages changes

--- paintings_store/paintings-store-customer/src/main/java/com/groupproject/microservice/customer/SpringRestDataConfig.java
+++ paintings_store/paintings-store-customer/src/main/java/com/groupproject/microservice/customer/SpringRestDataConfig.java
@@ -1,4 +1,4 @@
-package main.java.com.groupproject.microservice.customer;
+package com.groupproject.microservice.customer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
