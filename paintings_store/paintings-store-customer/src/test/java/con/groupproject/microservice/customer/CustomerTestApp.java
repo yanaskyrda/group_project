5ed7428df37e72324a3a7a6e597f69fc546b9f25
AntonHladkiy <54854877+AntonHladkiy@users.noreply.github.com>
2021-12-08 22:45:31 +0200

    ....

--- paintings_store/paintings-store-customer/src/test/java/con/groupproject/microservice/customer/CustomerTestApp.java
+++ paintings_store/paintings-store-customer/src/test/java/con/groupproject/microservice/customer/CustomerTestApp.java
@@ -1,4 +1,4 @@
-package con.groupproject.microservice.customer;
+package com.groupproject.microservice.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
--- paintings_store/paintings-store-customer/src/test/java/con/groupproject/microservice/customer/CustomerWebIntegrationTest.java
+++ paintings_store/paintings-store-customer/src/test/java/con/groupproject/microservice/customer/CustomerWebIntegrationTest.java
@@ -1,4 +1,4 @@
-package con.groupproject.microservice.customer;
+package com.groupproject.microservice.customer;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
